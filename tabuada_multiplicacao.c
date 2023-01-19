2c2d2b64cd803ab5ef2b0037b373cf4fc45d0ec7
Rafael <68768478+Rafaelfspenha@users.noreply.github.com>
2023-01-19 14:27:23 -0300

    Update tabuada_multiplicacao.c

--- tabuada_multiplicacao.c
+++ tabuada_multiplicacao.c
@@ -1,22 +1,24 @@
 // tabuada.c (MEU)
 #include <stdio.h>
-int main() {
-    int w,x,y,z,a;
+int main()
+{
+    int w, x, y, z, a;
     printf("tabuada\n\n");
     printf("Tabuada de: ");
-    scanf("%d",&x);
+    scanf("%d", &x);
     printf("\nDigite o número (inicial) de repetições: ");
-    scanf("%d",&w);
+    scanf("%d", &w);
     printf("\nDigite o (máximo) de repetições: ");
-    scanf("%d",&y);
+    scanf("%d", &y);
     printf("\n");
-    
-    a=0;
-    for(z=w;z<=y;z++){
-        printf("%d x %d = %d\n",x,z,x*z);
-        a+=x*z;
+
+    a = 0;
+    for (z = w; z <= y; z++)
+    {
+        printf("%d x %d = %d\n", x, z, x * z);
+        a += x * z;
     }
-    printf("\nValor total dos Resultados: %d\n",a);
-    
+    printf("\nValor total dos Resultados: %d\n", a);
+
     return 0;
 }
